4eec465aca7e81016b32aef3a45a04449c5af466
NeverALegend <20951141+NeverALegend@users.noreply.github.com>
2024-03-10 01:39:25 -0600

    Update .alias.zsh

--- dot_alias.zsh
+++ dot_alias.zsh
@@ -14,10 +14,6 @@ alias zshconfig='chezmoi edit -a ~/.zshrc' # chezmoi edit zshrc
 alias ohmyzsh='chezmoi edit -a ~/.oh-my-zsh' # chezmoi edit oh-my-zsh
 alias chezmoic='nvim ~/.config/chezmoi/chezmoi.toml' # chezmoi edit chezmoi config
 alias aliasc='chezmoi edit -a ~/.alias.zsh' # chezmoi edit alias.zsh
-alias hyprsource='chezmoi edit -a ~/.config/hypr/hyprland.conf' # chezmoi edit hyprland main source
-alias usrk='chezmoi edit -a ~/.config/hypr/keybindings.conf' # chezmoi edit hyprland keybindings
-alias usrw='chezmoi edit -a ~/.config/hypr/windowrules.conf' # chezmoi edit hyprland windowrules
-alias usrp='chezmoi edit -a ~/.config/hypr/userprefs.conf' # chezmoi edit hyprland userprefs
 alias nvimp='chezmoi edit -a ~/.config/nvim/init.lua' # chezmoi edit nvim init.lua
 alias kittyc='chezmoi edit -a ~/.config/kitty/kitty.conf' # chezmoi edit kitty config
 
@@ -51,13 +47,11 @@ function lg() {
 }
 
 # folder aliases || favorite folders
-alias pr='cd /mnt/projects' # projects folder
-alias sec='cd /mnt/projects/secret' #secret Github
-alias per='cd /mnt/projects/personal' # personal Github
-alias dots='cd /mnt/projects/dotfiles/' # "updated" dots
-alias test='cd /mnt/projects/test_play' # test playground
-alias notes='cd /mnt/projects/secret/legend_vault' # obsidian vault
-alias hypr='cd /mnt/projects/secret/legendhypr/' # hyprland script install
+alias pr='cd /Users/legend/projects' # projects folder
+alias sec='cd /Users/legend/projects/secret' #secret Github
+alias per='cd /Users/legend/projects/personal' # personal Github
+alias test='cd /Users/legend/projects/test_play' # test playground
+alias notes='cd /Users/legend/alter-ego' # obsidian vault
 
 	#Backup Data aliases
 
@@ -78,30 +72,14 @@ alias czap='chezmoi apply'
 alias czstat='chezmoi status'
 
 
-# Snapper aliases
-alias sl='snapper ls'
-alias sd='snapper delete' 
-alias single='snapper -c root create --description "***RECENT BASE CONFIG***"' 
-alias grub-up='sudo grub-mkconfig -o /boot/grub/grub.cfg'
-
-
-# aiImages aliases
-alias aiout='cd /mnt/projects/aiImages/Fooocus/outputs && ll'
-alias aistart='source /mnt/projects/aiImages/Fooocus/foocus/bin/activate && python /mnt/projects/aiImages/Fooocus/entry_with_update.py'
-
-
 # Misc
 alias e='exit'
 alias zsrc='source ~/.zshrc'
-alias clip='wl-copy'
-alias chd='cliphist wipe'
-alias cache='cd /mnt/projects/secret/legendhypr/Scripts/ && ./create_cache.sh -f'
-alias un='$aurhelper -Rns' # uninstall package
-alias up='$aurhelper -Syu' # update system/package/aur
-alias pl='$aurhelper -Qs' # list installed package
-alias ps='$aurhelper -Ss' # list availabe package
-alias pc='$aurhelper -Sc' # remove unused cache
-alias po='$aurhelper -Qtdq | $aurhelper -Rns -' # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
+alias un='brew uninstall' # uninstall package
+alias up='brew update' # update system/package/aur
+alias pl='brew list' # list installed package
+alias ps='brew search' # list availabe package
+alias pc='brew cleanup' # remove unused cache
 alias vc='code' # gui code editor
 
 
